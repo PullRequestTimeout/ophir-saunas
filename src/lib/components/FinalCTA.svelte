4ef2b3f71587a8eb001b2e6d34987705955aa3ea
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2026-03-24 09:11:36 -0700

    :construction: Build final CTA

---
+++ src/lib/components/FinalCTA.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import PrimaryButton from "$lib/components/PrimaryButton.svelte";
+
+	function handleBookingModal() {
+		// Placeholder function for booking modal logic
+		alert("Booking modal would appear here.");
+	}
+</script>
+
+<section>
+	<h2 class="decorative-heading">Ready to book your sauna?</h2>
+	<p class="subheading text-light">Flexible rentals. Easy setup. No stress.</p>
+	<div>
+		<PrimaryButton onAction={handleBookingModal} text="Book Now" />
+		<PrimaryButton href="/contact" text="Ask a Question" primary={false} />
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+		background-color: var(--color-background-tan);
+		gap: var(--spacing-m);
+	}
+
+	section p.subheading {
+		font-size: var(--font-body-l);
+	}
+
+	section > div {
+		display: flex;
+		gap: var(--spacing-m);
+		margin-top: var(--spacing-m);
+	}
+</style>
