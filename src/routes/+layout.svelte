4b119492c98572060a0e948fbd599fde99f5a1c6
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2026-03-24 09:16:25 -0700

    :construction: Added components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	let { children } = $props();
 	import Nav from "$lib/components/Nav.svelte";
+	import Footer from "$lib/components/Footer.svelte";
 	import "material-icons/iconfont/filled.css";
+	import FinalCTA from "$lib/components/FinalCTA.svelte";
 </script>
 
 <Nav />
 {@render children()}
+<FinalCTA />
+<Footer />
